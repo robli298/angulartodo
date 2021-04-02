ba66ff6fa211708d6a39fd19ccf8a35eb96d6fec
Robson Liebke <liebke@media-interactive.de>
2021-04-02 10:10:44 +0200

    git commit -m 'First commit'

---
+++ src/app/core/utils/array.utils.ts
@@ -0,0 +1,6 @@
+
+export class ArrayUtils {
+    public static getBy<T, P extends keyof T>(model: T[], prop: P, value: T[P]): T | null {
+        return model.filter(item => item[prop] === value)[0];
+    }
+}
