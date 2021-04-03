4f5b0a636bb70b2167b6adf0102ff251014813cb
Robson Liebke <liebke@media-interactive.de>
2021-04-03 19:28:38 +0200

    add list button

---
+++ src/app/features/tasks/components/tasks-list-workspace/tasks-list-workspace.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-list',
+  templateUrl: './tasks-list-workspace.component.html',
+  styleUrls: ['./tasks-list-workspace.component.scss']
+})
+export class TasksListWorkspaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addTaskList() {
+
+  }
+
+}
