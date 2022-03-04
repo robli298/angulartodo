da1d5530180b8e1994871af0b961392aca117f44
Robson Liebke <robsonliebke@gmail.com>
2022-03-05 00:53:11 +0100

    add virtual scrolling to list view

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { ListViewComponent } from './components/list-view/list-view.component';
 import { LetDirective } from './directives/let.directive';
 import { ListViewItemDirective } from './directives/list-view-item.directive';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 /**
  * This modules contains common components/pipes/directives and also exports commonly used Angular modules. This module
@@ -11,7 +12,8 @@ import { ListViewItemDirective } from './directives/list-view-item.directive';
 @NgModule({
     declarations: [LetDirective, ListViewComponent, ListViewItemDirective],
     imports: [
-        CommonModule
+        CommonModule,
+        ScrollingModule
     ],
     exports: [CommonModule, LetDirective, ListViewComponent, ListViewItemDirective]
 })
